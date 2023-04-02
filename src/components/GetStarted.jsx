7e7cc8edd29f3cad3eb4ba00f9a0e3eace907e0c
ri-commits <kartashourychard@gmail.com>
2023-04-02 14:40:11 +0300

    App is Finished ;)

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -4,7 +4,7 @@ import { arrowUp } from "../assets";
 const GetStarted = () => (
   <div
     className={` ${styles.flexCenter} w-[140px] h-[140px]
-  rounded-full bg-blue-gradient p-[2px] cursor-pointer `}
+  rounded-full bg-blue-gradient p-[2px] cursor-pointer hover:brightness-125`}
   >
     <div
       className={`${styles.flexCenter} flex-col bg-primary 
